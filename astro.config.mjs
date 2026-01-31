07673afb576058032ad376896c982d6b5f772bc0
Derek Zoladz <derek@derekzoladz.com>
2026-01-30 21:53:23 -0500

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,23 @@
-// @ts-check
-
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import Icons from "unplugin-icons/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	site: 'https://guitar.derekzoladz.com',
+	vite: {
+		plugins: [
+			tailwindcss(),
+			Icons({
+				compiler: 'astro',
+				autoInstall: true,
+			}),
+		],
+	},
+	integrations: [
+		mdx(),
+		sitemap(),
+	],
 });
