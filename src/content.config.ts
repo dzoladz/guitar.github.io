7f6b42bdc7c6c0a069875a9e37e9b9167752fc10
Derek Zoladz <derek@derekzoladz.com>
2026-02-07 22:17:03 -0500

    nine

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,2 +1,8 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
+
+const posts = defineCollection({
+    loader: glob({ pattern: "*.md", base: "src/pages/posts" }),
+});
+
+export const collections = { posts };
